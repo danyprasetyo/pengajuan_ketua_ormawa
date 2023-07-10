3724f21a87f0c07605eb519d5f806d0bef167d33
lunarcwhite <rizkym058@gmail.com>
2023-07-10 23:52:47 +0700

    p

--- resources/views/admin/pengajuan/print.blade.php
+++ resources/views/admin/pengajuan/print.blade.php
@@ -74,50 +74,6 @@
     </table>
     {{-- {{ public_path('storage/sertifikat/'.$pengajuan->sertifikat)} --}}
     <div id="viewerContainer"></div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.8.162/pdf.min.js"
-        integrity="sha512-9Wd08apcJEwm8g3lBTg1UW/njdN0iuuOVWKpyinK3uA7ISAE5PmEZ4y8bZYTXVOE3tlt7aFlCBBLmLt5cUxe2Q=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.8.162/pdf.worker.min.js"
-        integrity="sha512-kYruxZBxGQJy6pFwz9JVe6FCgCZEPPvxC3eoy4A+fCMWcSGqPxxGC7M1S6eyXCBfm/4d7l4cf8XNoULZQQ+MtQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.8.162/pdf_viewer.min.js"
-        integrity="sha512-4OIgtzKMwM3LvyUNlYaXtoeccZK3T+UGWdQ0rSnFj8B5uwgUFINQEMXPQIiBx7k2+bqAjl/A6LqYPzqRAj7Ewg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        // Path or URL to the PDF file
-        var pdfPath = `{{ url('storage/sertifikat/' . $pengajuan->sertifikat) }}`;
-
-        // Load the PDF file
-       const loadingTask = pdfjsLib.getDocument(pdfPath);
-            
-          await  loadingTask.promise.then((pdf) => {
-                // Fetch the first page of the PDF
-                pdf.getPage(1).then((page) => {
-                    // Set the desired scale (e.g., 1.5 for 150%)
-                    const scale = 1.5;
-                    
-                    // Set the canvas element to render the PDF page
-                    const canvas = document.createElement('canvas');
-                    const context = canvas.getContext('2d');
-                    canvas.width = page.view[2] * scale;
-                    canvas.height = page.view[3] * scale;
-                    // Render the PDF page into the canvas
-                    const renderContext = {
-                        canvasContext: context,
-                        viewport: page.getViewport({ scale: scale }),
-                    };
-                    page.render(renderContext).promise.then(() => {
-                        // Convert the canvas to an image
-                        const image = new Image();
-                        image.src = canvas.toDataURL('image/png');
-                        
-                        // Display the image on the page
-                        document.getElementById("tes").appendChild(image);
-
-                    });
-                });
-            });
-    </script>
 
 </body>
 
